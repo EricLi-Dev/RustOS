179e198c39cdb879de2127043eefbd0efc7f3f98
EricLi-Dev <johnlee888123456@gmail.com>
2024-10-24 10:41:38 -0400

    init rust crate

---
+++ target/debug/deps/rust_os-986e243d2b4d595e.d
@@ -0,0 +1,5 @@
+/Users/ericli/Documents/Projects/rust_os/target/debug/deps/rust_os-986e243d2b4d595e: src/main.rs
+
+/Users/ericli/Documents/Projects/rust_os/target/debug/deps/rust_os-986e243d2b4d595e.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rust_os.d
@@ -0,0 +1,1 @@
+/Users/ericli/Documents/Projects/rust_os/target/debug/rust_os: /Users/ericli/Documents/Projects/rust_os/src/main.rs
